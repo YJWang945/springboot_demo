b8baf9fc8954f5aea6a76ab4fdd14181455fc377
YJWang945 <168968582+YJWang945@users.noreply.github.com>
2024-08-23 14:48:34 +0800

    Update helloWorld.java

--- src/main/java/wyj/example/spingboot_demo01/Controller/helloWorld.java
+++ src/main/java/wyj/example/spingboot_demo01/Controller/helloWorld.java
@@ -5,10 +5,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/hello")
 public class helloWorld {
-    @RequestMapping("/helloWorld")
+    @RequestMapping("/hello")
     public String helloWorld() {
-        return "hello SpringBoot world update five";
+        return "hello SpringBoot world update six";
     }
 }
