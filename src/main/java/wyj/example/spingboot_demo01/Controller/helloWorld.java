152f6c6d966c64d846f99d166262e1f224223e6b
YJWang945 <168968582+YJWang945@users.noreply.github.com>
2024-08-26 14:33:46 +0800

    Update helloWorld.java

--- src/main/java/wyj/example/spingboot_demo01/Controller/helloWorld.java
+++ src/main/java/wyj/example/spingboot_demo01/Controller/helloWorld.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class helloWorld {
     @RequestMapping("/hello")
     public String helloWorld() {
-        return "hello World! ";
+        return "hello World Springboot! ";
     }
 }
